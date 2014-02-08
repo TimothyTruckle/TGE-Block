fd55123f8a5dea8a89591cc8b33165b7af12ea8c
TimothyTruckle <Timothy.Truckle@sourceforge.net>
2014-02-08 23:23:23 +0100

    Aufruf der nicht existierenden Klasse

--- src/test/java/net/bytewelt/tt/tgeblock/ZweiZahlenBerechnungTest.java
+++ src/test/java/net/bytewelt/tt/tgeblock/ZweiZahlenBerechnungTest.java
@@ -7,8 +7,8 @@ import org.junit.Test;
 public class ZweiZahlenBerechnungTest {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testAddiereKleineZahlen() {
+		new ZweiZahlenBerechnung();
 	}
 
 }
