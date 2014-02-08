333c741b86a9cdabef61eaedffc891b1938de61a
TimothyTruckle <Timothy.Truckle@sourceforge.net>
2014-02-08 23:48:27 +0100

    von IDE generierte Methode im Produktiv-Code
    Signed-off-by: TimothyTruckle <Timothy.Truckle@sourceforge.net>

--- src/main/java/net/bytewelt/tt/tgeblock/ZweiZahlenBerechnung.java
+++ src/main/java/net/bytewelt/tt/tgeblock/ZweiZahlenBerechnung.java
@@ -2,4 +2,9 @@ package net.bytewelt.tt.tgeblock;
 
 public class ZweiZahlenBerechnung {
 
+	public int ergebnisVon(int a, String rechenZeichen, int b) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
 }
