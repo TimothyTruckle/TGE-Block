4a4a3838f35937d7d43467cfca437eed41f4024c
TimothyTruckle <Timothy.Truckle@sourceforge.net>
2014-02-08 22:45:49 +0100

    von der IDE generierter Test

---
+++ src/test/java/net/bytewelt/tt/tgeblock/ZweiZahlenBerechnungTest.java
@@ -0,0 +1,14 @@
+package net.bytewelt.tt.tgeblock;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ZweiZahlenBerechnungTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
