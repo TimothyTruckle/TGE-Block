18eed848a19887874868fd54a2b87c309d9c2dea
TimothyTruckle <Timothy.Truckle@sourceforge.net>
2014-02-09 18:42:14 +0100

    Test verbessert um literalen Rückgabewert zu verhindern

--- src/main/java/net/bytewelt/tt/tgeblock/ZweiZahlenBerechnung.java
+++ src/main/java/net/bytewelt/tt/tgeblock/ZweiZahlenBerechnung.java
@@ -3,8 +3,7 @@ package net.bytewelt.tt.tgeblock;
 public class ZweiZahlenBerechnung {
 
 	public int ergebnisVon(int a, String rechenZeichen, int b) {
-		// TODO Auto-generated method stub
-		return 3;
+		return a+b;
 	}
 
 }
--- src/test/java/net/bytewelt/tt/tgeblock/ZweiZahlenBerechnungTest.java
+++ src/test/java/net/bytewelt/tt/tgeblock/ZweiZahlenBerechnungTest.java
@@ -2,16 +2,24 @@ package net.bytewelt.tt.tgeblock;
 
 import static org.junit.Assert.*;
 
+import java.util.Random;
+
 import org.junit.Test;
 
 public class ZweiZahlenBerechnungTest {
 
+	private static final Random RANDOM = new Random();
+
 	@Test
 	public void testAddiereKleineZahlen() {
-		int a = 1;
-		String rechenZeichen= "+";
-		int b = 2;
-		int ergebnis = new ZweiZahlenBerechnung().ergebnisVon(a,rechenZeichen,b);
+		ZweiZahlenBerechnung zweiZahlenBerechnung = new ZweiZahlenBerechnung();
+		for (int i = 0; i < 10; i++) {
+			int a = RANDOM.nextInt(10);
+			int b = RANDOM.nextInt(10);
+			assertEquals("ergebnis", a+b,
+					zweiZahlenBerechnung.ergebnisVon(a, "+", b));
+
+		}
 	}
 
 }
